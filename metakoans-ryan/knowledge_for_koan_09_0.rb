0ddcbc6c697775b835128e034ffa288513927670
Ryan Kinderman <ryan@kinderman.net>
2007-04-29 23:58:27 +0000

    got koan 9 to pass, need to refactor a bit

--- metakoans-ryan/knowledge_for_koan_09_0.rb
+++ metakoans-ryan/knowledge_for_koan_09_0.rb
@@ -26,20 +26,26 @@ class Module
     
     [name, default_value]
   end
+
+  def attr_assigned?(attr_name)
+    instance_variable_get(attr_name + "_assigned")
+  end
   
   def define_methods(name, default_value, &block)
     attribute_name = "@#{name}"
     define_method name do
       default_value = instance_eval &block unless block.nil?
-      instance_variable_get(attribute_name) || default_value
+      attribute_assigned = !instance_variable_get(attribute_name + "_assigned").nil?
+      instance_variable_get(attribute_name) || (attribute_assigned ? nil : default_value)
     end
     define_method name + "?" do
       !send(name).nil?
     end
     define_method name + "=" do |value|
       instance_variable_set attribute_name, value
-      default_value = nil
-      block = nil
+      instance_variable_set attribute_name + "_assigned", true
     end
   end
+  
+  
 end
